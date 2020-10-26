e4e57de79100feb8ce0f9b3ee8d26fe7824a985f
Alessandro Almeida <43619109+asalmeidarj@users.noreply.github.com>
2020-10-26 15:24:04 -0300

    Add files via upload

---
+++ monte_carlo_method/03_integral.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def func(x):
+    return abs(np.sin(x))
+
+npontos = 500000
+x = np.random.rand(npontos)*2*np.pi
+y = np.random.rand(npontos)
+
+xg = list()  # Coordenada x do ponto abaixo do gráfico
+yg = list()  # Coordenada y do ponto abaixo do gráfico
+
+xe = list()  # Coordenada x do ponto acima do gráfico
+ye = list()  # Coordenada y do ponto acima do gráfico
+
+area_retangulo = 2*np.pi
+
+"""Determinar os pontos abaixo e acima do gráfico"""
+ocorrencias = 0
+for i in range(npontos):
+    if y[i] < func(x[i]):
+        xg.append(x[i])
+        yg.append(y[i])
+        ocorrencias += 1
+    else:
+        xe.append(x[i])
+        ye.append(y[i])
+
+plt.figure(figsize=(7,8))
+plt.scatter(xg, yg, s=0.8)
+plt.scatter(xe, ye, s=0.8)
+plt.title(f'A integral no intervalo [0, 2pi] de seno é {ocorrencias/npontos*area_retangulo}')
+plt.show()
+
+
+
