4440a1e2df42e99a8d22812d1bde85e8af2faad5
Alessandro Almeida <asalmeidarj@gmail.com>
2020-10-30 19:28:23 -0300

    Derivada de uma função em um ponto dado

---
+++ serie_taylor/derivada_aprox.py
@@ -0,0 +1,20 @@
+"""1° derivada de uma função f em um ponto p por aproximação"""
+
+import numpy as np
+from sympy import *
+
+def ndeltx(num):
+    return 10**(-num)
+
+def recebe():
+    expr = input('Derivada da função: ')
+    ponto = input('no ponto: ')
+    return (sympify(expr), sympify(ponto))
+
+f, p = recebe()
+deltx = ndeltx(5)
+print(f'f = {f}')
+print(f'p = {p}')
+print(f'deltx = {deltx}')
+result = (f.subs('x', p + deltx) - f.subs('x', p - deltx))/(2*deltx)
+print(result)
